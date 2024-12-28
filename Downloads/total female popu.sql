72c7941da2646aac8aea4a9182f2836f4a8e119b
sivarise <sivadurusala@gmail.com>
2024-12-28 13:50:44 -0500

    Delete Downloads directory

--- Downloads/total female popu.sql
+++
@@ -1,5 +0,0 @@
-SELECT zipcode, SUM(CASE WHEN gender = 'FEMALE' THEN population ELSE 0 END) AS female_population
-FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
-GROUP BY zipcode
-ORDER BY female_population DESC
-LIMIT 5;
