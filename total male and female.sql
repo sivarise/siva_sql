95d1a67caeab5f5c195dc4024db409d793ac9822
sivarise <sivadurusala@gmail.com>
2025-01-02 19:11:52 -0500

    Delete total male and female.sql

--- total male and female.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
-  SUM(CASE WHEN gender = 'Male' THEN population ELSE 0 END) AS male_population,
-  SUM(CASE WHEN gender = 'Female' THEN population ELSE 0 END) AS female_population
-FROM `bigquery-public-data.census_bureau_usa.population_by_zip_2010`
-WHERE CAST(zipcode AS STRING) = '94085';
